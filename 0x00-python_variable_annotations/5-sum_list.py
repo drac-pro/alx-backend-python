349965d2bf22db98f5c74a70a4f62325315a63a7
Darius Tohtin <dariustohtin@gmail.com>
2024-07-04 20:59:57 +0100

    edited imports

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """defines a type-annotated function sum_list"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """calculates the sum of element in a list of floats
     Args:
         input_list(list[float]): list of floating point numbers
