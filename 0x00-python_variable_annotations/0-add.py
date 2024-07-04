176d849136ae0e038d7d8c83dbd5aa4699ffe000
Darius Tohtin <dariustohtin@gmail.com>
2024-07-04 18:09:05 +0100

    task 0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a type anotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """adds two floating point numbers
+    Args:
+        a(float): num one
+        b(float): num two
+    Return:
+        float: sum of a + b
+    """
+    return a + b
