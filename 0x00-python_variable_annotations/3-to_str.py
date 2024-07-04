13cdecbb4988309f344c93a67b63e9203df30dd9
Darius Tohtin <dariustohtin@gmail.com>
2024-07-04 20:34:34 +0100

    task 3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""defines a type-annotated function to_str"""
+
+
+def to_str(n: float) -> str:
+    """represents a float as string
+    Args:
+        n(float): a floating point number
+    Return:
+        str: string representation of the float
+    """
+    return str(n)
