5e8b3a2a5b8edfdbe73713415afa6b08e428639f
Darius Tohtin <dariustohtin@gmail.com>
2024-07-08 21:40:08 +0100

    added sorting to the result

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """defines an asynchronous coroutine wait_n"""
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawn wait_random n times with the specified max_delay
     Args:
         n(int): number of times to spawn wait_random
         max_delay: maximum delay
     """
-    routines = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*routines)
-    return delays
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
