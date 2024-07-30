cb6a755f0edfec6a6a1ea6f0a17b69ea67b0509a
Darius Tohtin <dariustohtin@gmail.com>
2024-07-30 03:52:28 +0100

    task 6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,6 +4,7 @@ import unittest
 from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import (GithubOrgClient)
+TEST_PAYLOAD = __import__('fixtures').TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -30,3 +31,18 @@ class TestGithubOrgClient(unittest.TestCase):
                 'repos_url': 'https://api.github.com/users/google/repos'}
             self.assertEqual(GithubOrgClient("google")._public_repos_url,
                              'https://api.github.com/users/google/repos')
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """tests public_repos method"""
+        test_payload = TEST_PAYLOAD[0]
+        mock_get_json.return_value = test_payload[1]
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_payload[0]['repos_url']
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(),
+                test_payload[2]
+            )
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
